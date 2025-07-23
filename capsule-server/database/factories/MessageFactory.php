15939c772c79c4c0876b2e5c0950a85dfdacaa26
hibaanadani <hibaanadani1@gmail.com>
2025-07-24 00:43:37 +0300

    fixed the factory to genearte 70% of the times an image

--- capsule-server/database/factories/MessageFactory.php
+++ capsule-server/database/factories/MessageFactory.php
@@ -9,19 +9,19 @@ class MessageFactory extends Factory
 {
     public function definition(): array
     {
-        return [
-            'user_id' => rand(1,30),
-            'title'=>fake()->sentence(3),
-            'message' => fake()->paragraph(),
-            'mood' => fake()->randomElement(['happy', 'sad', 'angry', 'excited', 'calm']),
-            'image' => fake()->imageUrl(640, 480, 'messages', true, null, false, 'png'),
-            'audio' => fake()->url(),
-            'color' => fake()->colorName(),
-            'reveal_date' => fake()->dateTimeBetween('-2 years', '+2 years'),
-            'privacy' => fake()->randomElement(['private', 'public', 'limited']),
-            'surprise_mode' => fake()->boolean(),
-            'location' => fake()->city() . ', ' . fake()->country(),
-            'ipaddress' => fake()->ipv4(),
-        ];
+    return [
+        'user_id' => rand(1,30),
+        'title' => fake()->sentence(3),
+        'message' => fake()->paragraph(),
+        'mood' => fake()->randomElement(['happy', 'sad', 'angry', 'excited', 'calm']),
+        'image' => fake()->boolean(70) ? fake()->imageUrl(640, 480, 'messages', true, null, false, 'png') : null,
+        'audio' => fake()->url(),
+        'color' => fake()->colorName(),
+        'reveal_date' => fake()->dateTimeBetween('-2 years', '+2 years'),
+        'privacy' => fake()->randomElement(['private', 'public', 'limited']),
+        'surprise_mode' => fake()->boolean(),
+        'location' => fake()->city() . ', ' . fake()->country(),
+        'ipaddress' => fake()->ipv4(),
+    ];
     }
 }
